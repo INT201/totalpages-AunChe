9c51441531881b8e7a694185c83579ffe0687870
AunChe <aunchalee.test@mail.kmutt.ac.th>
2022-09-14 20:52:05 +0700

    test

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,13 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
-}
+  if (items === null || items === undefined) 
+  return undefined
+} 
+ if ( rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0)
+  return 1
+
+
+
+
 module.exports = totalPages
