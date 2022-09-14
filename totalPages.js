99662f9881b7c9c4dbc2c811beae460556639e3a
AunChe <aunchalee.test@mail.kmutt.ac.th>
2022-09-14 21:25:11 +0700

    test

--- totalPages.js
+++ totalPages.js
@@ -1,12 +1,15 @@
 const { template } = require('@babel/core')
 
-function totalPages(arrayItems, rowsPerPage) {
-  if (items === null || items === undefined) 
-  return undefined
-} 
- if ( rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0)
-  return 1
 
+  function totalPages(arrayItems, rowsPerPage) {
+    if (arrayItems === undefined || arrayItems === null) {
+      return undefined
+    } else if (rowsPerPage === undefined || rowsPerPage === null || rowsPerPage == 0) {
+      return 1 ;
+    } else {
+      return Math.ceil(arrayItems.length / rowsPerPage) ;
+    }
+  }
 
 
 
